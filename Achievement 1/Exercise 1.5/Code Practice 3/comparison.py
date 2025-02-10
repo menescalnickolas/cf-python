16673bcaeb4313570df562072e10cb895a1ca837
menescalnickolas <menescalnickolas@gmail.com>
2025-02-10 04:43:25 +0100

    Attempted Code Practice 3

---
+++ Achievement 1/Exercise 1.5/Code Practice 3/comparison.py
@@ -0,0 +1,38 @@
+class Height:
+  def __init__(self, feet, inches): 
+    self.feet = feet
+    self.inches = inches
+
+  def __lt__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A < height_inches_B
+
+  def __le__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A <= height_inches_B
+
+  def __eq__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A == height_inches_B
+  
+  def __gt__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A > height_inches_B
+  
+  def __ge__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A >= height_inches_B
+  
+  def __ne__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A != height_inches_B
+  
+print(Height(4, 6) > Height(4, 5))
+print(Height(4, 5) >= Height(4, 5))
+print(Height(5, 9) != Height(5, 10))
